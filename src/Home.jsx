b384ce38cfdaf906a8c5503c213e2d1993453d0b
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-10 12:24:24 +0200

    feat:initial setup

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="max-w-5xl mx-auto">
+      <h1 className="text-7xl font-bold">React coding exercices</h1>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
