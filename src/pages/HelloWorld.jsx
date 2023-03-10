7c4bbcb06d96de38361bbac95a27eae4d31206a6
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-10 12:34:17 +0200

    feat: adding the hello world

---
+++ src/pages/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-[#FEC800]">
+      <h1 className="">Hello world !</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
