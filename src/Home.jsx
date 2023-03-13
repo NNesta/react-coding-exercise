1a3ff33a155f1528ed33b21dc20796645f0d3ba8
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-13 19:51:59 +0200

    fix: optimizing codes

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className=" h-screen ">
       <h1 className="text-7xl font-bold text-center">React coding exercices</h1>
       <ul className=" text-center flex items-center justify-center h-full">
-        <li className="">
+        <li>
           <a href="/clickme">user click exercise</a>
         </li>
       </ul>
--- src/pages/Captures.jsx
+++ src/pages/Captures.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Captures = () => {
   return (
     <div className="max-w-xl mx-auto h-screen">
