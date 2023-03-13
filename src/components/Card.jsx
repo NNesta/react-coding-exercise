71ae989117836ee7895f4586887f62ec2d54987d
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-13 18:20:47 +0200

    feat:adding web api to the cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div className=" bg-white w-[400px] h-[350px]">
+      <div class="flip-card h-full">
+        <div class="flip-card-inner h-full">
+          <div class="flip-card-back  my-12">
+            <div className="text-2xl text-black font-bold text-center h-full">
+              More information here
+            </div>
+          </div>
+          <div class="flip-card-front text-black flex flex-col gap-2 ">
+            <img
+              className="object-cover w-52 h-52 mx-auto"
+              src={props.avatar}
+              alt=""
+            />
+            <p className="text-xl font-semibold">{props.fullName}</p>
+            <p>{props.title}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
