2814b7f0b4b9e71dd3298dadc175df5a50e2d27b
Ngabonziza Nestor <ngabonest@gmail.com>
2023-03-13 16:34:52 +0200

    feat:adding form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Form from "./pages/Form";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/form" element={<Form />} />
     </Routes>
   );
 };
